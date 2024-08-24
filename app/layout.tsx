1986b1d848dba2e39565403ef998c7e109e879dd
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-24 21:05:37 +0300

    configured layout for redux

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
+// app/layout.tsx (no "use client")
 import { ReactNode } from "react";
-// import NavBar from "./components/NavBar";
 import "../public/static/css/global.css";
 import { Roboto } from "next/font/google";
-// import Footer from "./components/Footer";
+import ClientProvider from "./ClientProvider"; // New component
 
 export const metadata = {
   title: "FurniSphere",
 };
+
 const roboto = Roboto({
   weight: "400",
   subsets: ["latin"],
@@ -18,11 +19,8 @@ export default function RootLayout({ children }: { children: ReactNode[] }) {
       <body
         className={`${roboto.className} mx-auto flex w-full flex-col scrollbar scrollbar-track-primary  scrollbar-thumb-secondary md:w-3/4`}
       >
-        {/* <NavBar /> */}
-        <hr />
-        {children}
-
-        {/* <Footer /> */}
+        <ClientProvider>{children}</ClientProvider>{" "}
+        {/* Move Redux logic here */}
       </body>
     </html>
   );
