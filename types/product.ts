303ceecffcb3ccfafac2bcc0b2f14b48444e5ede
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-24 18:20:30 +0300

    finazlized product type

---
+++ types/product.ts
@@ -0,0 +1,24 @@
+// types/product.ts
+
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  category_id: number;
+  image: string;
+  color: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface Product3D {
+  id: number;
+  product_id: number;
+  model_file_path: string;
+  file_type: "gltf" | "obj" | "fbx";
+  created_at: string;
+  updated_at: string;
+}
+
+export type PaginatedProducts = Pagination<Product>;
