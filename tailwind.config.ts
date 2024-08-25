1d71cdbec464b79b82ac55c55074f78f92b3af2c
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-25 22:25:27 +0300

    added background image and add dasyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,12 +9,10 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "hero-bg": "url('/static/images/heroImage.png')",
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
 export default config;
