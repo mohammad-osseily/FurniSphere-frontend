2fd8dd6b32f48cda6f811a09e44f9b64a63d2826
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-26 23:41:09 +0300

    finalized page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
-import Image from "next/image";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import SuggestedProducts from "./components/SuggestedProducts";
+import ProductCategories from "./components/ProductCategories";
+import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <>
+      <Hero />
+      <Features />
+      <SuggestedProducts />
+      <ProductCategories />
+      <HowItWorks />
+      <Footer />
+    </>
   );
 }
