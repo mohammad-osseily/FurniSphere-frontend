ba21bcb3219b5afce89ee1485159895aa40934bc
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-26 23:42:18 +0300

    finalized footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 py-8 text-white">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2024 FurniSphere. All rights reserved.</p>
+        <p>Information | Privacy Policy | Terms of Service</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
