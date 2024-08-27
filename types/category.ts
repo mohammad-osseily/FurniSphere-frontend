8255d37094cc2c18c925630f291e1ff69b1e6cad
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-27 20:35:47 +0300

    fixed category type

--- types/category.ts
+++ types/category.ts
@@ -1,9 +1,11 @@
 // types/category.ts
 import { Pagination } from "./pagination";
+import { Product } from "./product";
 
 export interface Category {
   id: number;
   name: string;
+  products?: Product[]; // Associating products with the category
   created_at: string;
   updated_at: string;
 }
