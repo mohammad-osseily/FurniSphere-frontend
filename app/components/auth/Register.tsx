7c2422d12de956b95cee329f85757a76676a1d72
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-28 00:41:51 +0300

    finalized register

--- app/components/auth/Register.tsx
+++ app/components/auth/Register.tsx
@@ -2,12 +2,11 @@
 
 import React from "react";
 import { useForm } from "react-hook-form";
-import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
-import { RootState } from "../../store/store";
-import { registerUser } from "../../store/slices/authSlice";
 import Swal from "sweetalert2";
 import "sweetalert2/dist/sweetalert2.min.css";
+import { registerUser } from "../../services/authServices";
+import Link from "next/link";
 
 interface RegisterFormInputs {
   name: string;
@@ -17,20 +16,16 @@ interface RegisterFormInputs {
 }
 
 const Register: React.FC = () => {
-  const dispatch = useDispatch();
   const router = useRouter();
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<RegisterFormInputs>();
-  const { user, loading, error } = useSelector(
-    (state: RootState) => state.auth
-  );
 
   const onSubmit = async (data: RegisterFormInputs) => {
     try {
-      await dispatch(registerUser(data)).unwrap();
+      await registerUser(data.name, data.email, data.password);
 
       Swal.fire({
         title: "Success!",
@@ -38,7 +33,7 @@ const Register: React.FC = () => {
         icon: "success",
         confirmButtonText: "OK",
       }).then(() => {
-        router.push("/");
+        router.push("/login");
       });
     } catch (err: unknown) {
       if (err && typeof err === "object" && "message" in err) {
@@ -54,52 +49,55 @@ const Register: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 space-y-3 bg-white rounded shadow-md">
-        <h1 className="text-2xl font-bold text-center">Register</h1>
-        {error && <p className="text-red-500 text-center">{error}</p>}
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-          <div>
-            <label className="block mb-1 text-gray-600">Name</label>
+    <div className="flex justify-center bg-login-bg items-center min-h-screen bg-cover bg-center">
+      <div className="w-[600px] bg-white bg-opacity-50 backdrop-blur-xl shadow-lg text-black rounded-lg p-8 font-semibold">
+        <h1 className="text-3xl text-left">Register</h1>
+        <form onSubmit={handleSubmit(onSubmit)} className="mt-8 space-y-6">
+          <div className="relative pb-5">
+            <div className="pb-2">Name</div>
             <input
               type="text"
+              placeholder="Enter your name"
               {...register("name", { required: "Name is required" })}
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+              className="w-full h-12 bg-accent border border-primary border-opacity-20 rounded-xl px-6 text-black placeholder-neutral outline-none"
             />
             {errors.name && (
               <p className="text-red-500 text-sm">{errors.name.message}</p>
             )}
           </div>
-          <div>
-            <label className="block mb-1 text-gray-600">Email</label>
+          <div className="relative pb-5">
+            <div className="pb-2">Email</div>
             <input
               type="email"
+              placeholder="Enter your email"
               {...register("email", { required: "Email is required" })}
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+              className="w-full h-12 bg-accent border border-primary border-opacity-20 rounded-xl px-6 text-black placeholder-neutral outline-none"
             />
             {errors.email && (
               <p className="text-red-500 text-sm">{errors.email.message}</p>
             )}
           </div>
-          <div>
-            <label className="block mb-1 text-gray-600">Password</label>
+          <div className="relative pb-5">
+            <div className="pb-2">Password</div>
             <input
               type="password"
+              placeholder="Enter your password"
               {...register("password", { required: "Password is required" })}
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+              className="w-full h-12 bg-accent border border-primary border-opacity-20 rounded-xl px-6 text-black placeholder-neutral outline-none"
             />
             {errors.password && (
               <p className="text-red-500 text-sm">{errors.password.message}</p>
             )}
           </div>
-          <div>
-            <label className="block mb-1 text-gray-600">Confirm Password</label>
+          <div className="relative pb-5">
+            <div className="pb-2">Confirm Password</div>
             <input
               type="password"
+              placeholder="Confirm your password"
               {...register("password_confirmation", {
                 required: "Please confirm your password",
               })}
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+              className="w-full h-12 bg-accent border border-primary border-opacity-20 rounded-xl px-6 text-black placeholder-neutral outline-none"
             />
             {errors.password_confirmation && (
               <p className="text-red-500 text-sm">
@@ -107,20 +105,24 @@ const Register: React.FC = () => {
               </p>
             )}
           </div>
-          <button
-            type="submit"
-            disabled={loading}
-            className="w-full py-2 text-white bg-blue-500 rounded hover:bg-blue-600 disabled:opacity-50"
-          >
-            {loading ? "Registering..." : "Register"}
-          </button>
+
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className="w-36 h-12 bg-primary text-white rounded-full shadow-lg mt-4 hover:opacity-90 hover:text-white"
+            >
+              Register
+            </button>
+          </div>
         </form>
-        <p className="text-sm text-center text-gray-600">
-          Already have an account?{" "}
-          <a href="/login" className="text-blue-500 hover:underline">
-            Login
-          </a>
-        </p>
+        <div className="text-center text-sm mt-6">
+          <p>
+            Already have an account?{" "}
+            <Link href="/login" className="hover:underline">
+              Login
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
