6ee67fee7ed4fdbc3c2a2e30e916538d60ae2d0c
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-29 23:11:15 +0300

    finalaized modal

--- app/components/ProductModal.tsx
+++ app/components/ProductModal.tsx
@@ -22,7 +22,50 @@ const ProductModal = ({ product, isOpen, onClose }) => {
   };
 
   return (
-    
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+      <div className="modal-box flex h-2/3 w-full max-w-5xl items-center bg-white   md:w-1/2">
+        <div className="flex h-full w-full flex-col md:flex-row">
+          <div className="flex w-full flex-row items-center justify-center md:w-1/2 md:flex-col">
+            <img
+              className="h-3/4 w-3/4"
+              src={product.image}
+              width={100}
+              height={50}
+              alt={product.name}
+            />
+          </div>
+
+          <div className="flex h-full w-full flex-col pt-4 md:w-1/2">
+            <div className="pb-4 text-4xl text-black md:text-5xl">
+              {product.name}
+            </div>
+            <div className="pb-4 text-2xl text-black md:text-3xl">
+              {product.price}$
+            </div>
+
+            <div className="pt-4 text-2xl text-black md:text-3xl">
+              Description
+            </div>
+            <div className="pt-4 text-lg text-black md:text-2xl break-words">
+              {product.description}
+            </div>
+
+            <button
+              onClick={handleAddToCart}
+              className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+            >
+              Add to Cart
+            </button>
+          </div>
+          <div
+            className="cursor-pointer pb-6 pt-6 text-3xl md:pb-0 md:text-4xl"
+            onClick={onClose} // Close modal on clicking the arrow
+          >
+            <IoArrowBack />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
