9ac20617a3e06cdebfe0be7b1b9a4272b037884e
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-29 22:49:16 +0300

    finaliazed login

--- app/components/auth/Login.tsx
+++ app/components/auth/Login.tsx
@@ -44,4 +44,58 @@ const Login: React.FC = () => {
     }
   };
 
+  return (
+    <div className="flex justify-center bg-login-bg items-center min-h-screen bg-cover bg-center">
+      <div className="w-[600px] bg-white bg-opacity-50 backdrop-blur-xl shadow-lg text-black rounded-lg p-8">
+        <h1 className="text-3xl text-left">Login</h1>
+        <form onSubmit={handleSubmit(onSubmit)} className="mt-8 space-y-6">
+          <div className="relative pb-5">
+            <div className="pb-2">Email</div>
+            <input
+              type="email"
+              placeholder="Email"
+              {...register("email", { required: "Email is required" })}
+              className="w-full h-12 bg-accent border border-primary border-opacity-20 rounded-xl px-6 text-black placeholder-neutral outline-none"
+            />
+            <BsPerson className="absolute right-4 top-[55%] transform -translate-y-1/2 text-xl text-black" />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email.message}</p>
+            )}
+          </div>
+          <div className="relative">
+            <div className="pb-2">Password</div>
+            <input
+              type="password"
+              placeholder="Enter your password"
+              {...register("password", { required: "Password is required" })}
+              className="w-full h-12 bg-accent border border-primary border-opacity-20 rounded-xl px-6 text-black placeholder-neutral outline-none"
+            />
+            <BsLock className="absolute right-4 top-[55%] transform-translate-y-1/2 text-xl text-black" />
+            {errors.password && (
+              <p className="text-red-500 text-sm">{errors.password.message}</p>
+            )}
+          </div>
 
+          <div className=" flex justify-center">
+            <button
+              type="submit"
+              className="w-36 h-12 bg-primary text-white rounded-full shadow-lg mt-4 hover:opacity-90 hover:text-white"
+            >
+              Log in
+            </button>
+          </div>
+        </form>
+        <div className="text-center text-sm mt-6">
+          <p>
+            Don't have an account?{" "}
+            <Link href="/register" className=" hover:underline">
+              Register
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
