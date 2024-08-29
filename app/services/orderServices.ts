aa9c70f22248df1a69802ec6468c9e31ab1e5adb
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-29 23:19:08 +0300

    save token

--- app/services/orderServices.ts
+++ app/services/orderServices.ts
@@ -1,19 +1,10 @@
-// services/orderServices.ts
-import axios from "axios";
+// app/services/authServices.ts
+import axios from 'axios';
 
-const API_URL = "http://127.0.0.1:8000/api";
+const API_URL = 'http://127.0.0.1:8000/api'; // Your API base URL
 
-export const getOrderHistory = async () => {
-  try {
-    const token = localStorage.getItem("token");
-    const response = await axios.get(`${API_URL}/orders/history`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    return response.data.orders;
-  } catch (error) {
-    console.error("Error fetching order history:", error);
-    throw error;
-  }
+// Save token to local storage
+export const saveTokenToLocalStorage = (token: string) => {
+    localStorage.setItem('token', token);
 };
+
