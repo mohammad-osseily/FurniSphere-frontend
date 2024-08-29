44b2cc0c3850ad3c4da508dd08891d12d51f36a8
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-29 22:47:17 +0300

    added function

--- app/components/CheckoutPage.tsx
+++ app/components/CheckoutPage.tsx
@@ -10,3 +10,15 @@ const CheckoutPage = () => {
   const [comment, setComment] = useState("");
   const router = useRouter();
 
+  const handleOrderSubmit = async (e : any) => {
+    e.preventDefault();
+    try {
+      await createOrder(addressLine, city, comment);
+      alert("Order placed successfully!");
+      router.push("/profile"); // Navigate to the profile page after order
+    } catch (error) {
+      console.error("Failed to place order:", error);
+      alert("Failed to place the order.");
+    }
+  };
+
