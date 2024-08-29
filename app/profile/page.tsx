befd75f4860c370a3ab508d6d80611a59d2902de
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-08-29 23:15:51 +0300

    finalized profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,47 +1,8 @@
-// app/profile/page.tsx
-"use client";
+// app/pages/profile/index.tsx
 
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { getUserProfile } from "../services/authServices";
+import ProfilePage from "../components/ProfilePage";
 
-const UserProfile: React.FC = () => {
-  const [user, setUser] = useState(null);
-  const router = useRouter();
 
-  useEffect(() => {
-    const fetchUserProfile = async () => {
-      const storedUser = await getUserProfile();
-      if (!storedUser) {
-        router.push("/login");
-      } else {
-        setUser(storedUser);
-      }
-    };
-
-    fetchUserProfile();
-  }, []);
-
-  if (!user) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Profile</h1>
-      <div className="bg-white p-4 rounded shadow-md">
-        <p>
-          <strong>Name:</strong> {user.name}
-        </p>
-        <p>
-          <strong>Email:</strong> {user.email}
-        </p>
-        <p>
-          <strong>Role:</strong> {user.role}
-        </p>
-      </div>
-    </div>
-  );
-};
-
-export default UserProfile;
+export default function Profile() {
+  return <ProfilePage />;
+}
