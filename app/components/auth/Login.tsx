c4fc8886cbdce192d4c96090977df99c69b51e0f
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-09-03 14:41:21 +0300

    fixed desgin in login

--- app/components/auth/Login.tsx
+++ app/components/auth/Login.tsx
@@ -79,9 +79,9 @@ const Login: React.FC = () => {
           <div className=" flex justify-center">
             <button
               type="submit"
-              className="w-36 h-12 bg-primary text-white rounded-full shadow-lg mt-4 hover:opacity-90 hover:text-white"
+              className="w-36 h-12 bg-primary text-white rounded-xl shadow-lg mt-4 hover:opacity-90 hover:text-white"
             >
-              Log in
+              LogIn
             </button>
           </div>
         </form>
