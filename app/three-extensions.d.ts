2aea21c1844e2df098ad1120d1bf5209b51fc49f
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-09-05 19:54:27 +0300

    added GltFLoader

--- app/three-extensions.d.ts
+++ app/three-extensions.d.ts
@@ -54,3 +54,31 @@ declare module 'three/examples/jsm/controls/OrbitControls' {
   }
 }
 
+declare module 'three/examples/jsm/loaders/GLTFLoader' {
+  import { Loader, LoadingManager, Group } from 'three';
+
+  export class GLTFLoader extends Loader {
+      constructor(manager?: LoadingManager);
+
+      load(
+          url: string,
+          onLoad: (gltf: GLTF) => void,
+          onProgress?: (event: ProgressEvent) => void,
+          onError?: (event: ErrorEvent) => void
+      ): void;
+
+      parse(
+          data: ArrayBuffer | string,
+          path: string,
+          onLoad: (gltf: GLTF) => void
+      ): void;
+  }
+
+  export interface GLTF {
+      scene: Group;
+      scenes: Group[];
+      animations: any[];
+      cameras: any[];
+      asset: any;
+  }
+}
