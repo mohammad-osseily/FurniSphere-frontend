51a1c1ff556afda213db979df76464db92725044
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-09-05 19:51:14 +0300

    added camer setup

---
+++ app/components/ThreeScene.tsx
@@ -0,0 +1,21 @@
+'use client';
+import { useRef, useEffect } from 'react';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const ThreeScene = () => {
+  const mountRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color('white');
+
+    // Camera setup
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(0, 10, 20);
+
+};
+
+export default ThreeScene;
