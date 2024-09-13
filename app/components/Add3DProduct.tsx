e87bb5858d7fdc30a64cf0e532d87d30357df7f4
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-09-14 01:00:04 +0300

    added awit function

--- app/components/Add3DProduct.tsx
+++ app/components/Add3DProduct.tsx
@@ -26,6 +26,21 @@ const Add3DProduct = () => {
       rotation,
     };
 
+    try {
+      await create3DProduct(formData); // Call service to create the 3D product
+      setMessage('3D product added successfully!');
+      setProductId('');
+      setModelFilePath('');
+      setPosition({ x: 0, y: 0, z: 0 });
+      setScale({ x: 3, y: 3, z: 3 });
+      setRotation({ x: 0, y: 0, z: 0 });
+    } catch (error) {
+      setMessage('Error adding 3D product. Please try again.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
 
 };
 
