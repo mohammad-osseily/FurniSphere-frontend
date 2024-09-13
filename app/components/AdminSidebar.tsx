3e259c5eb520dbd14a00421cc1c3284d5a53f1f0
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-09-14 01:04:20 +0300

    added adminSidebar

---
+++ app/components/AdminSidebar.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import React from 'react'
+import { Box, List, ListItem, ListItemText, ListItemIcon, Divider, Button } from '@mui/material'
+import PeopleIcon from '@mui/icons-material/People'
+import AddBoxIcon from '@mui/icons-material/AddBox'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
+import ThreeDRotationIcon from '@mui/icons-material/ThreeDRotation'
+import LogoutIcon from '@mui/icons-material/Logout'
+import Link from 'next/link'
+import Image from 'next/image'
+import { logoutUser } from '../services/authServices'
+import { useRouter } from "next/navigation";
+
+
+const drawerWidth = 240
+
+export default function AdminSidebar() {
+  const router = useRouter();
+  
+  const handleLogout = () => {
+    logoutUser();
+    
+    router.push("/login");
+  };
+
+  return (
+    <Box
+      sx={{
+        height: '100vh', // Set height to 100% of viewport height
+        backgroundColor: 'oklch(0.3354 0.090487 241.152)',
+        color: 'white',
+        width: drawerWidth,
+        flexShrink: 0,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        '& .MuiDrawer-paper': { width: drawerWidth, boxSizing: 'border-box' },
+      }}
+    >
+    
+    </Box>
+  )
+}
