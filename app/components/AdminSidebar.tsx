8240945b47f6986552670ef07b80cbc1beae4a3d
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-09-19 10:28:03 +0300

    finalized admin sidebar

--- app/components/AdminSidebar.tsx
+++ app/components/AdminSidebar.tsx
@@ -1,45 +1,52 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { Box, List, ListItem, ListItemText, ListItemIcon, Divider, Button } from '@mui/material'
-import PeopleIcon from '@mui/icons-material/People'
-import AddBoxIcon from '@mui/icons-material/AddBox'
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
-import ThreeDRotationIcon from '@mui/icons-material/ThreeDRotation'
-import LogoutIcon from '@mui/icons-material/Logout'
-import Link from 'next/link'
-import Image from 'next/image'
-import { logoutUser } from '../services/authServices'
+import React from "react";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  Divider,
+  Button,
+} from "@mui/material";
+import PeopleIcon from "@mui/icons-material/People";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import ThreeDRotationIcon from "@mui/icons-material/ThreeDRotation";
+import LogoutIcon from "@mui/icons-material/Logout";
+import Link from "next/link";
+import Image from "next/image";
+import { logoutUser } from "../services/authServices";
 import { useRouter } from "next/navigation";
 
-
-const drawerWidth = 240
+const drawerWidth = 240;
 
 export default function AdminSidebar() {
   const router = useRouter();
-  
+
   const handleLogout = () => {
     logoutUser();
-    
+
     router.push("/login");
   };
 
   return (
     <Box
       sx={{
-        height: '100vh', // Set height to 100% of viewport height
-        backgroundColor: 'oklch(0.3354 0.090487 241.152)',
-        color: 'white',
+        height: "100vh", // Set height to 100% of viewport height
+        backgroundColor: "oklch(0.3354 0.090487 241.152)",
+        color: "white",
         width: drawerWidth,
         flexShrink: 0,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        '& .MuiDrawer-paper': { width: drawerWidth, boxSizing: 'border-box' },
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        "& .MuiDrawer-paper": { width: drawerWidth, boxSizing: "border-box" },
       }}
     >
       {/* Logo */}
-      <Box sx={{ pt: 2 ,textAlign: 'center', margin: "0 auto "}} >
+      <Box sx={{ pt: 2, textAlign: "center", margin: "0 auto " }}>
         <Image
           src="/static/images/logo2.png"
           alt="Logo"
@@ -48,14 +55,12 @@ export default function AdminSidebar() {
         />
       </Box>
 
-      
-
       {/* Menu Items */}
-      <Box sx={{ flexGrow: 1, overflow: 'auto' , pt:2}}>
+      <Box sx={{ flexGrow: 1, overflow: "auto", pt: 2 }}>
         <List>
           <Link href="/admin-dashboard" passHref legacyBehavior>
-            <ListItem  component="a">
-              <ListItemIcon sx={{ color: 'white' }}>
+            <ListItem component="a">
+              <ListItemIcon sx={{ color: "white" }}>
                 <PeopleIcon />
               </ListItemIcon>
               <ListItemText primary="User Management" />
@@ -63,8 +68,8 @@ export default function AdminSidebar() {
           </Link>
 
           <Link href="/admin-dashboard/add-product" passHref legacyBehavior>
-            <ListItem  component="a">
-              <ListItemIcon sx={{ color: 'white' }}>
+            <ListItem component="a">
+              <ListItemIcon sx={{ color: "white" }}>
                 <AddBoxIcon />
               </ListItemIcon>
               <ListItemText primary="Add Products" />
@@ -72,8 +77,8 @@ export default function AdminSidebar() {
           </Link>
 
           <Link href="/admin-dashboard/orders" passHref legacyBehavior>
-            <ListItem  component="a">
-              <ListItemIcon sx={{ color: 'white' }}>
+            <ListItem component="a">
+              <ListItemIcon sx={{ color: "white" }}>
                 <ShoppingCartIcon />
               </ListItemIcon>
               <ListItemText primary="Order Management" />
@@ -81,8 +86,8 @@ export default function AdminSidebar() {
           </Link>
 
           <Link href="/admin-dashboard/threeD" passHref legacyBehavior>
-            <ListItem  component="a">
-              <ListItemIcon sx={{ color: 'white' }}>
+            <ListItem component="a">
+              <ListItemIcon sx={{ color: "white" }}>
                 <ThreeDRotationIcon />
               </ListItemIcon>
               <ListItemText primary="3D Manipulation" />
@@ -90,8 +95,6 @@ export default function AdminSidebar() {
           </Link>
         </List>
       </Box>
-
-      
 
       {/* Logout Button */}
       <Box sx={{ p: 2 }}>
@@ -101,9 +104,9 @@ export default function AdminSidebar() {
           fullWidth
           startIcon={<LogoutIcon />}
           sx={{
-            backgroundColor: '#FF6347',
-            '&:hover': {
-              backgroundColor: '#ff4c33',
+            backgroundColor: "#FF6347",
+            "&:hover": {
+              backgroundColor: "#ff4c33",
             },
           }}
           onClick={handleLogout}
@@ -112,5 +115,5 @@ export default function AdminSidebar() {
         </Button>
       </Box>
     </Box>
-  )
+  );
 }
