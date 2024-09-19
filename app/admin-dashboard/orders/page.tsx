72c2e3e38ee434b69cdb5f87fb2b2958ded9ce1b
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-09-19 10:26:38 +0300

    finalized admin order

--- app/admin-dashboard/orders/page.tsx
+++ app/admin-dashboard/orders/page.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import AdminSidebar from "../../components/AdminSidebar"  // Import the AdminSidebar
-import OrderManagement from '../../components/OrderMangment';  // Your existing OrderManagement component
-import { Box } from '@mui/material';
+import React from "react";
+import AdminSidebar from "../../components/AdminSidebar"; // Import the AdminSidebar
+import OrderManagement from "../../components/OrderMangment"; // Your existing OrderManagement component
+import { Box } from "@mui/material";
 
 const OrdersPage = () => {
   return (
-    <Box sx={{ display: 'flex', height:"100%" }}>
+    <Box sx={{ display: "flex", height: "100%" }}>
       {/* Render Sidebar */}
       <AdminSidebar />
 
       {/* Main Content */}
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <div className='text-3xl font-semibold pb-10'>Order Mangement</div>
+        <div className="text-3xl font-semibold pb-10">Order Mangement</div>
         <OrderManagement />
       </Box>
     </Box>
