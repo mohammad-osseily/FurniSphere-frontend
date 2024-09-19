310a20773f78ce5eec7742b9eb7e9154507751ea
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-09-19 10:32:40 +0300

    finalized api

--- app/services/api.ts
+++ app/services/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "http://13.36.244.88/backend/api",
 });
 
 API.interceptors.request.use((config) => {
