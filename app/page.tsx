1b68d823ab93a5ac1af87566e1739a9890cc5b78
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-09-19 10:25:20 +0300

    finalized home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
-import SuggestedProducts from "./components/SuggestedProducts";
 import ProductCategories from "./components/ProductCategories";
 import HowItWorks from "./components/HowItWorks";
 import Footer from "./components/Footer";
+import Recommendations from "./components/Recommendations";
 
 export default function HomePage() {
   return (
-    <>
+    <div className="w-full">
       <Hero />
       <Features />
-      <SuggestedProducts />
+      <Recommendations />
       <ProductCategories />
       <HowItWorks />
       <Footer />
-    </>
+    </div>
   );
 }
