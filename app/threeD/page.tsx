64e16e01c8cdb9f58ebe6ed5cf8caa35487c4692
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-09-19 10:34:34 +0300

    finalized threeD

--- app/threeD/page.tsx
+++ app/threeD/page.tsx
@@ -1,10 +1,10 @@
 // pages/threeScene.tsx
-import React from 'react';
-import ThreeScene from '../components/ThreeScene'; // Adjust the import path as necessary
+import React from "react";
+import ThreeScene from "../components/ThreeScene"; // Adjust the import path as necessary
 
 const ThreeScenePage = () => {
   return (
-    <div style={{ height: '100vh' }}>
+    <div style={{ height: "100vh" }}>
       <ThreeScene />
     </div>
   );
